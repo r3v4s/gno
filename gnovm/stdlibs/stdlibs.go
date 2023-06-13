edd5d2d3563573f7cda2fca05172abca53c98494
n3wbie <r3v4@onbloc.xyz>
2023-06-13 11:51:35 +0900

    apply PrevRealm()

--- gnovm/stdlibs/stdlibs.go
+++ gnovm/stdlibs/stdlibs.go
@@ -143,26 +143,61 @@ func InjectPackage(store gno.Store, pn *gno.PackageNode) {
 		pn.DefineGoNativeValue("IntSize", strconv.IntSize)
 		pn.DefineGoNativeValue("AppendUint", strconv.AppendUint)
 	case "internal/std":
-		pn.DefineNative("GetOrigCaller",
+		pn.DefineNative("PrevRealm",
 			gno.Flds( // params
 			),
 			gno.Flds( // results
-				"", "Address",
+				"", "Realm",
 			),
 			func(m *gno.Machine) {
-				ctx := m.Context.(ExecContext)
+				var (
+					ctx = m.Context.(ExecContext)
+					// Default lastCaller is OrigCaller, the signer of the tx
+					lastCaller  = ctx.OrigCaller
+					lastPkgPath = ""
+				)
+
+				for i := m.NumFrames() - 1; i > 0; i-- {
+					fr := m.Frames[i]
+					if fr.LastPackage == nil || !fr.LastPackage.IsRealm() {
+						// Ignore non-realm frame
+						continue
+					}
+					pkgPath := fr.LastPackage.PkgPath
+					// The first realm we encounter will be the one calling
+					// this function; to get the calling realm determine the first frame
+					// where fr.LastPackage changes.
+					if lastPkgPath == "" {
+						lastPkgPath = pkgPath
+					} else if lastPkgPath == pkgPath {
+						continue
+					} else {
+						lastCaller = fr.LastPackage.GetPkgAddr().Bech32()
+						lastPkgPath = pkgPath
+						break
+					}
+				}
+
+				// Empty the pkgPath if we return a user
+				if ctx.OrigCaller == lastCaller {
+					lastPkgPath = ""
+				}
+
+				// Return the result
 				res0 := gno.Go2GnoValue(
 					m.Alloc,
 					m.Store,
-					reflect.ValueOf(ctx.OrigCaller),
+					reflect.ValueOf(Realm{
+						addr:    lastCaller,
+						pkgPath: lastPkgPath,
+					}),
 				)
-				addrT := store.GetType(gno.DeclaredTypeID("std", "Address"))
-				res0.T = addrT
+
+				realmT := store.GetType(gno.DeclaredTypeID("std", "Realm"))
+				res0.T = realmT
 				m.PushValue(res0)
 			},
 		)
-
-		// XXX PrevRealm() https://github.com/gnolang/gno/pull/667
 	case "std":
 		// NOTE: some of these are overridden in tests/imports.go
 		// Also see stdlibs/InjectPackage.
